2a2d1e3627b339f5ca0940f1a9daa64712151980
KerliK <kerliruul@gmail.com>
2017-05-09 21:58:07 +0300

    map2 0905

--- map 0905_1/main.go
+++ map 0905_1/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	a := uks("lollpea")
 	fmt.Println(a)
+	h, k := kolm(a)
+	fmt.Println(h, k)
 }
 
 func uks(uk string) map[string]int {
@@ -14,4 +16,14 @@ func uks(uk string) map[string]int {
 	}
 
 	return kuu
+}
+func kolm(neli map[string]int) ([]string, []int) {
+	paike := []string{}
+	maa := []int{}
+	for k, v := range neli {
+		paike = append(paike, k)
+		maa = append(maa, v)
+	}
+
+	return paike, maa
 }
